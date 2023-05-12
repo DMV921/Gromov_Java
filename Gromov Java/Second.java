75da6cf18b8ea4a689de251674bff52ab629a03b
DMV921 <104264067+DMV921@users.noreply.github.com>
2023-05-13 01:35:54 +0300

    initial commit

---
+++ Gromov Java/Second.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Second {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in, "Cp866");
+        System.out.println("Введите имя");
+        String string = s.nextLine();
+        if (string.equals("Вячеслав")) {
+            System.out.println("Привет " + string);
+        } else {
+            System.out.println("Нет такого имени");
+        }
+    }
+}
